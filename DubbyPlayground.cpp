91308744f89bc39545f278a991c44e5c4c61ca9d
simonrostami <simonrostami@gmail.com>
2024-06-03 12:38:33 +0200

    filter not working

--- DubbyPlayground.cpp
+++ DubbyPlayground.cpp
@@ -11,20 +11,18 @@ Dubby dubby;
 
 // SYNTH
 VariableShapeOscillator osc1, osc2;
-
-
+ LadderFilter filter;
 
 const int NUM_PAGES = 4; // assuming 4 types of drums: bass, snare, tom, hihat
-
 
 // Vector of vectors to store whether each knob is within tolerance for each drum
 // Booleans to track tolerance for each knob of each drum
 
-const char *algorithmTitles[NUM_PAGES] = {"OSC. 1", "OSC 2", "MIDI CTRL P3", "MIDI CTRL P4"};
+const char *algorithmTitles[NUM_PAGES] = {"OSC. 1", "OSC 2", "FILTER", "AMP"};
 const char *customLabels[NUM_PAGES][NUM_KNOBS] = {
     {"SHAPE", "OCTAVE", "FINE", "AMPL."},
     {"SHAPE", "OCTAVE", "FINE", "AMPL."},
-    {"PRM 9", "PRM 10", "PRM 11", "PRM 12"},
+    {"CUTOFF", "RES", "DRIVE", "ENV"},
     {"PRM 13", "PRM 14", "PRM 15", "PRM 16"}};
 
 float savedKnobValues[NUM_PAGES][NUM_KNOBS] = {
@@ -47,8 +45,11 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, s
         {
             float _in = SetGains(dubby, j, i, in, out);
 
+            float oscOut = osc1.Process() + osc2.Process();
+
+            float filterOut = filter.Process(oscOut);
             // === AUDIO CODE HERE ===================
-                out[0][i] =out[1][i] =out[2][i] = out[3][i] = (osc1.Process() + osc2.Process()) *0.1f;
+            out[0][i] = out[1][i] = out[2][i] = out[3][i] = oscOut * 0.1f;
             // =======================================
 
             CalculateRMS(dubby, _in, out[j][i], j, sumSquared);
@@ -69,7 +70,6 @@ int main(void)
 
     dubby.seed.StartAudio(AudioCallback);
 
-
     osc1.Init(dubby.seed.AudioSampleRate());
     osc1.SetWaveshape(0);
     osc1.SetSync(false);
@@ -77,26 +77,44 @@ int main(void)
     osc2.Init(dubby.seed.AudioSampleRate());
     osc2.SetWaveshape(0);
     osc2.SetSync(false);
+
+    filter.Init(dubby.seed.AudioSampleRate());
+
+    LadderFilter::FilterMode currentMode = LadderFilter::FilterMode::LP24;
+
     while (1)
     {
         Monitor(dubby);
         MonitorMidi();
         handleKnobs(dubby, algorithmTitles, customLabels, savedKnobValues);
 
-osc1.SetPW(savedKnobValues[0][0]);
-osc1.SetSyncFreq(440.f*savedKnobValues[0][2]);
+        osc1.SetPW(savedKnobValues[0][0]);
+        osc1.SetSyncFreq(440.f * savedKnobValues[0][2]);
 
+        osc2.SetPW(savedKnobValues[1][0]);
+        osc2.SetSyncFreq(440.f * savedKnobValues[1][2]);
 
-osc2.SetPW(savedKnobValues[1][0]);
-osc2.SetSyncFreq(440.f*savedKnobValues[1][2]);
+        // FILTER
+        float inGain = savedKnobValues[2][2] * 4;
+        float res = savedKnobValues[2][1];
+        float cutOffKnobValue = savedKnobValues[2][0];
 
+        // Map the knob value to a logarithmic scale for cutoff frequency
+        float minCutoff = .05f;    // Minimum cutoff frequency in Hz
+        float maxCutoff = 17000.f; // Maximum cutoff frequency in Hz
+        float mappedCutoff = daisysp::fmap(cutOffKnobValue, minCutoff, maxCutoff, daisysp::Mapping::LOG);
 
-        
+        // float roundedCutoff = round(cutOffKnobValue * 1000 + 0.5f)*20.f;
 
-  
+        // Update the filter parameters
+        filter.SetInputDrive(inGain);
+        filter.SetRes(res);
+        filter.SetFreq(mappedCutoff);
 
-                      if(dubby.buttons[3].TimeHeldMs() > 300){dubby.ResetToBootloader();}
-
+        if (dubby.buttons[3].TimeHeldMs() > 300)
+        {
+            dubby.ResetToBootloader();
+        }
     }
 }
 
