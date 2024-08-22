6905a227778368af08d7a8c7ecdea014a6c5e78c
simonrostami <simonrostami@gmail.com>
2024-08-22 14:09:02 +0200

    separate paramlistvalues

--- Dubby.cpp
+++ Dubby.cpp
@@ -51,6 +51,13 @@ using namespace daisy;
 #define MIDILIST_SPACING 8
 #define MIDILIST_ROWS_ON_SCREEN 5
 
+#define PARAMLIST_X_START 1
+#define PARAMLIST_X_END 123
+#define PARAMLIST_Y_START 11
+#define PARAMLIST_Y_END 19
+#define PARAMLIST_SPACING 8
+#define PARAMLIST_ROWS_ON_SCREEN 5
+
 #define ENCODER_LONGPRESS_THRESHOLD 300
 
 int currentBitmapIndex = 2; // Initial bitmap index
@@ -73,10 +80,10 @@ void Dubby::Init()
 
     for (int i = 0; i < MIDILIST_ROWS_ON_SCREEN; i++)
     {
-        paramListBoxBounding[i][0] = MIDILIST_X_START;
-        paramListBoxBounding[i][1] = MIDILIST_Y_START + i * MIDILIST_SPACING;
-        paramListBoxBounding[i][2] = MIDILIST_X_END;
-        paramListBoxBounding[i][3] = MIDILIST_Y_END + i * MIDILIST_SPACING;
+        midiListBoxBounding[i][0] = MIDILIST_X_START;
+        midiListBoxBounding[i][1] = MIDILIST_Y_START + i * MIDILIST_SPACING;
+        midiListBoxBounding[i][2] = MIDILIST_X_END;
+        midiListBoxBounding[i][3] = MIDILIST_Y_END + i * MIDILIST_SPACING;
     }
 
     scrollbarWidth = int(128 / WIN_LAST);
@@ -859,15 +866,15 @@ void Dubby::DisplayMidiSettingsList(int increment)
 
     for (int i = optionStart, j = 0; i < optionStart + MIDILIST_ROWS_ON_SCREEN; i++, j++)
     {
-        display.DrawRect(paramListBoxBounding[j][0], paramListBoxBounding[j][1], paramListBoxBounding[j][2], paramListBoxBounding[j][3], false, true);
+        display.DrawRect(midiListBoxBounding[j][0], midiListBoxBounding[j][1], midiListBoxBounding[j][2], midiListBoxBounding[j][3], false, true);
 
         if (midiSettingSelected == i)
         {
             if (isMidiSettingSelected)
-                display.DrawRect(paramListBoxBounding[j][0], paramListBoxBounding[j][1], paramListBoxBounding[j][2], paramListBoxBounding[j][3], true, true);
+                display.DrawRect(midiListBoxBounding[j][0], midiListBoxBounding[j][1], midiListBoxBounding[j][2], midiListBoxBounding[j][3], true, true);
 
             int x = 3;
-            display.DrawCircle(x, paramListBoxBounding[j][1] + 4, 1, !isMidiSettingSelected);
+            display.DrawCircle(x, midiListBoxBounding[j][1] + 4, 1, !isMidiSettingSelected);
         }
 
         display.SetCursor(5, MIDILIST_Y_START + 2 + (j * MIDILIST_SPACING));
--- Dubby.h
+++ Dubby.h
@@ -313,6 +313,7 @@ namespace daisy
         const int windowBoxBounding[3][4] = {{0, 53, 43, 61}, {43, 53, 85, 61}, {85, 53, 127, 61}};
         int menuListBoxBounding[5][4];
         int paramListBoxBounding[5][4];
+        int midiListBoxBounding[5][4];
 
         int scrollbarWidth = 0;
         int barSelector = 0;
